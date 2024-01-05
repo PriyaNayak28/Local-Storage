5b77a79ea261fdbc412216473d2ef81bb736d2f5
PriyaNayak28 <nayakpriya612@gmail.com>
2024-01-05 20:52:01 +0530

    4th assignment dlt element

--- local.js
+++ local.js
@@ -36,24 +36,23 @@ function saveToLocalStorage(event) {
     // let myObj = JSON.parse(localStorage.getItem('user'));
 
 
-
-
     localStorage.setItem(userDetails.EMAIL, JSON.stringify(userDetails));
     // let myObj = JSON.parse(localStorage.getItem('user'));
-
-
-
-
-
-
 
 
     const newLi = document.createElement('li');
     const ul = document.getElementById('listitem');
 
     //newLi.innerHTML = `Name: ${myObj.NAME}, Email: ${myObj.EMAIL}, Phone: ${myObj.PHONE}`;
-    newLi.innerHTML = `Name: ${userDetails.NAME}, Email: ${userDetails.EMAIL}, Phone: ${userDetails.PHONE}`;
+    newLi.innerHTML = `Name: ${userDetails.NAME}, Email: ${userDetails.EMAIL}, Phone: ${userDetails.PHONE}` + '<button class="delete-btn">delete</button>';
     ul.appendChild(newLi);
+
+    ul.addEventListener('click', function (event) {
+        if (event.target.classList.contains('delete-btn')) {
+            deleteItem = event.target.parentElement;
+            ul.removeChild(deleteItem);
+        }
+    })
 
 
 
