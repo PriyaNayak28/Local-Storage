ea35c332077d4ec3e3acdf11baafee5a4baa6b74
PriyaNayak28 <nayakpriya612@gmail.com>
2024-01-05 20:35:37 +0530

    3rd assignment

--- local.js
+++ local.js
@@ -1,5 +1,22 @@
 
-// document.addEventListener('DOMContentLoaded', function () {
+// function saveToLocalStorage(event) {
+//     event.preventDefault();
+
+//     const NAME = event.target.username.value;
+//     const EMAIL = event.target.emailId.value;
+//     const PHONE = event.target.phoneNo.value;
+
+//     let userDetails = {
+//         NAME,
+//         EMAIL,
+//         PHONE,
+//     }
+
+//     let userDetail = JSON.stringify(userDetails);
+//     localStorage.setItem('userDetails', userDetail);
+//     // let myObj = JSON.parse(localStorage.getItem('userDetails'));
+// };
+
 
 function saveToLocalStorage(event) {
     event.preventDefault();
@@ -14,12 +31,45 @@ function saveToLocalStorage(event) {
         PHONE,
     }
 
-    let userDetail = JSON.stringify(userDetails);
-    localStorage.setItem('userDetails', userDetail);
-    // let myObj = JSON.parse(localStorage.getItem('userDetails'));
+    // let userDetail = JSON.stringify(userDetails);
+    // localStorage.setItem('user', userDetail);
+    // let myObj = JSON.parse(localStorage.getItem('user'));
+
+
+
+
+    localStorage.setItem(userDetails.EMAIL, JSON.stringify(userDetails));
+    // let myObj = JSON.parse(localStorage.getItem('user'));
+
+
+
+
+
+
+
+
+    const newLi = document.createElement('li');
+    const ul = document.getElementById('listitem');
+
+    //newLi.innerHTML = `Name: ${myObj.NAME}, Email: ${myObj.EMAIL}, Phone: ${myObj.PHONE}`;
+    newLi.innerHTML = `Name: ${userDetails.NAME}, Email: ${userDetails.EMAIL}, Phone: ${userDetails.PHONE}`;
+    ul.appendChild(newLi);
+
 
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
